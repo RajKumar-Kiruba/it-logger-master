dfbe619ec7f2b3de651cb153c36dbe4b36551182
RajKumar-Kiruba <raj.kirubanantham@atherenergy.com>
2022-02-11 10:33:42 +0530

    Updates

---
+++ precache-manifest.2057a257e3d0882f88869c801e6ccb2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb3c756a2801a562812cb0cb88f1088",
+    "url": "/it-logger-master/index.html"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/it-logger-master/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "89c58694c459cfa00b60",
+    "url": "/it-logger-master/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/it-logger-master/static/js/2.fadd1756.chunk.js"
+  },
+  {
+    "revision": "89c58694c459cfa00b60",
+    "url": "/it-logger-master/static/js/main.05838891.chunk.js"
+  },
+  {
+    "revision": "9fc8b50a195ed899b7c6",
+    "url": "/it-logger-master/static/js/runtime~main.ea00f4a8.js"
+  }
+]);
